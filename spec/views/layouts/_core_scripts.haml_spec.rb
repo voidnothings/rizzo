5add54c327b65da645d69f0a8a3b2fb39e7c8991
TA Tyree <tatyree@gmail.com>
2012-05-24 13:23:05 +0100

    [#28512409] The previous version didn't like the double call.  This sort it.

--- spec/views/layouts/_core_scripts.haml_spec.rb
+++ spec/views/layouts/_core_scripts.haml_spec.rb
@@ -2,13 +2,9 @@ require 'spec_helper'
 
 describe "layouts/partials/_core_scripts" do
   
-  it "builds path to rizzo.js using asset_path" do
-    view.should_receive(:javascript_path).with("rizzo.js")
-    render
-  end
-  
-  it "builds a script tag for the google js api" do
-    view.should_receive(:javascript_path).with("http://www.google.com/jsapi?key=ABQIAAAADUr8Vd6I7bfZ5k4c27F7KxR5cxXriAJsP5a75Cx4cnHTXGWMNxQxhFddQkNg7EBCllU86qgA_ugglg")
+  it "builds path to rizzo.js and the google js_api using asset_path" do
+    view.should_receive(:javascript_path).once.with("rizzo.js")
+    view.should_receive(:javascript_path).once.with("http://www.google.com/jsapi?key=ABQIAAAADUr8Vd6I7bfZ5k4c27F7KxR5cxXriAJsP5a75Cx4cnHTXGWMNxQxhFddQkNg7EBCllU86qgA_ugglg")
     render
   end
   
