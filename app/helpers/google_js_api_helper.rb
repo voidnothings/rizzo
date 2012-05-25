162069afd023b88a026a1ef75f09ace8008d6c05
TA Tyree <tatyree@gmail.com>
2012-05-25 12:58:26 +0100

    Do not load the google jsapi in Rizzo.
    Async loading here causes problems loading the map scripts in Waldorf.  It needs to be loaded directly in Waldorf.
    
    [#28512409]

--- app/helpers/google_js_api_helper.rb
+++ app/helpers/google_js_api_helper.rb
@@ -1,7 +1,21 @@
 module GoogleJsApiHelper
-  
+
   def js_api_script(js_api_key = 'ABQIAAAADUr8Vd6I7bfZ5k4c27F7KxR5cxXriAJsP5a75Cx4cnHTXGWMNxQxhFddQkNg7EBCllU86qgA_ugglg')
     javascript_path "http://www.google.com/jsapi?key=#{js_api_key}"
   end
-    
+
+  def load_detail_map_script
+    return<<-END
+    google.load("maps", 3.9, {other_params:"sensor=false"});
+    google.setOnLoadCallback(function() {
+            var options = {
+                  zoom: 10,
+                  center: new google.maps.LatLng(-26.616507,-56.409463),
+                  mapTypeId: google.maps.MapTypeId.ROADMAP
+            }
+            map = new google.maps.Map(document.getElementById('map_canvas'), options);
+    });
+    END
+  end
+
 end
--- spec/views/layouts/_core_scripts.haml_spec.rb
+++ spec/views/layouts/_core_scripts.haml_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe "layouts/partials/_core_scripts" do
-  
+
   it "builds path to rizzo.js and the google js_api using asset_path" do
     view.should_receive(:javascript_path).once.with("rizzo.js")
-    view.should_receive(:javascript_path).once.with("http://www.google.com/jsapi?key=ABQIAAAADUr8Vd6I7bfZ5k4c27F7KxR5cxXriAJsP5a75Cx4cnHTXGWMNxQxhFddQkNg7EBCllU86qgA_ugglg")
     render
   end
-  
+
 end
