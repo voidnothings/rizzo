0137f7602af0980e75496175101393c619ef527b
dave@kapoq.com <dave@kapoq.com>
2012-06-19 15:44:00 +0100

    Return original url if encrypted_url is longer than about 2000 chars

--- spec/helpers/redirector_helper_spec.rb
+++ spec/helpers/redirector_helper_spec.rb
@@ -1,17 +1,50 @@
 require 'spec_helper'
 
 describe RedirectorHelper do
-  describe "#redirector_path" do
+  describe "#redirect_to" do
     let(:url)           { "url" }
     let(:encrypted_url) { "encrypted url" }
+    let(:length)        { double "length" }
+
+    before do
+      Rizzo::UrlEncryptor.stub(encrypt: encrypted_url)
+      encrypted_url.stub(length: length)
+      length.stub(:< => true)
+      Stats = double unless defined? Stats
+      Stats.stub(:timing)
+    end
     
     it "encrypts the url" do
       Rizzo::UrlEncryptor.should_receive(:encrypt).with(url).and_return(encrypted_url)
-      helper.redirector_path(url)
+      helper.redirector_to(url)
     end
 
-    it "returns a path to the redirector controller show action for the encrypted url" do
-      helper.redirector_path(url).should =~ /\/r/#{url}/
+    it "checks the length of the encrypted url against MAX_URL_LENGTH" do
+      encrypted_url.should_receive(:length).and_return(length)
+      length.should_receive(:<).with(RedirectorHelper::MAX_URL_LENGTH)
+      helper.redirector_to(url)
+    end
+
+    it "increments the encrypted_url_too_long stat" do
+      Stats.should_receive(:timing).with("redirector.encrypted_url_length", length)
+      helper.redirector_to(url)
+    end
+    
+    context "when the encrypted url is reasonably short" do
+      it "returns a path to the redirector controller show action for the encrypted url" do
+        helper.redirector_to(url).should =~ /\/r/#{url}/
+      end
+    end
+
+    context "when the encrypted url is too long" do
+      before do
+        encrypted_url.stub(length: length)
+        length.stub(:< => false)
+      end
+
+      it "returns target_url" do
+        helper.redirector_to(url).should eq url
+      end
     end
   end
 end
