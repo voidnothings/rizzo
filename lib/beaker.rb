717be5d398702fcf036919c00f56d9eb117eea60
Ian Feather <info@ianfeather.co.uk>
2012-06-20 13:41:13 +0100

    Gem now working as part of the asset pipeline

--- lib/beaker.rb
+++ lib/beaker.rb
@@ -1,5 +1,15 @@
 require "beaker/version"
 
 module Beaker
-  Rails.config.assets.paths << File.expand_path("../framework", __FILE__)
+  module Rails
+    class Engine < ::Rails::Engine
+
+      # initializer "beaker.configure_rails_initialization" do |app|
+      # 
+      #   Sass::Plugin.add_template_location File.join(Gem.loaded_specs['beaker'].full_gem_path, '/app/assets/stylesheets')
+      # 
+      # end
+
+    end
+  end
 end
--- lib/beaker/version.rb
+++ lib/beaker/version.rb
@@ -1,3 +1,5 @@
 module Beaker
-  VERSION = "0.0.1"
+  module Rails
+    VERSION = "0.0.1"
+  end
 end
