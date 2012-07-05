55cbbf2bb8d09e1acc4c4d995520b07e63761b58
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-07-05 17:09:12 +0100

    remove js assets, prepare avocado entry

--- app/assets/javascripts/rizzo.js
+++
@@ -1,8 +0,0 @@
-//= require jquery-1.7.1.min.js
-//= require jquery-plugins.js
-//= require jquery.cookies.2.2.0.min.js
-//= require lib/lp_core.js
-//= require lib/lp_authentication.coffee
-//= require lib/lp_app.js.erb
-
-// require lib/mill.min.js
--- rizzo.gemspec
+++ rizzo.gemspec
@@ -28,4 +28,5 @@ Gem::Specification.new do |gem|
   gem.add_development_dependency 'pickle'
   gem.add_development_dependency 'cucumber-rails'
   gem.add_development_dependency 'rake'
+  gem.add_development_dependency 'avocado'
 end
