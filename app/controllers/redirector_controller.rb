40137e340f44b69d98981029fca1caf3bb0e0ecd
Paul Grayson <paul.grayson@gmail.com>
2012-07-09 11:01:07 +0100

    Extract redirector_controller increment* methods so they can be used outside redirector_controller

--- app/controllers/redirector_controller.rb
+++ app/controllers/redirector_controller.rb
@@ -10,20 +10,8 @@ class RedirectorController < ActionController::Base
     render :status => :bad_request, :nothing => true
   end
 
-  private
+private
 
-  def increment_stats_bucket_for_bad_redirected_url(url = "blank")
-    increment_stats_bucket("redirector", "bad_url", url)
-  end
+  include RedirectorSupport
 
-  def increment_stats_bucket_for_redirected_url(url)
-    uri  = URI.parse(url)
-    host = uri.hostname.gsub(/\./, "-")
-    path = uri.path.gsub(/\//, ".")
-    increment_stats_bucket("redirector", "#{host}#{path}")
-  end
-
-  def increment_stats_bucket(*bucket_parts)
-    Stats.increment(bucket_parts.join(".")) if defined?(Stats)
-  end
 end
