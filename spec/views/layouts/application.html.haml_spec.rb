ca1bde29f79cfdbceda53bd4c2bf1a70d5c76cd2
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-07-13 17:27:33 +0100

    update spec descriptions on application spec

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -14,14 +14,14 @@ describe "layouts/application" do
     rendered.should include tag
   end
 
-  it "yields the content for scripts" do
+  it "yields the content for place_hotels_link" do
     tag = "place hotels link"
     view.content_for(:place_hotels_link){tag}
     render
     rendered.should include tag
   end
 
-  it "yields the content for scripts" do
+  it "yields the content for meta_content" do
     tag = "meta_content"
     view.content_for(:meta){tag}
     render
