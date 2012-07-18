1627fd91f4100c155841b623286c2a365f3967f4
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-07-18 14:34:35 +0100

    remove spec helepers from /public to  spec/

--- spec/javascripts/helpers/SpecHelper.js
+++ spec/javascripts/helpers/SpecHelper.js
@@ -2,7 +2,11 @@ require.config({
   paths:{
     jquery: "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
     lib: 'public/assets/javascripts/lib',
-    vendor: 'vendor/assets/javascripts',
+    handlebars: 'vendor/assets/javascripts/handlebars',
+    underscore: 'vendor/assets/javascripts/underscore',
     jplugs: "jquery/plugins"
   }
 });
+
+
+
