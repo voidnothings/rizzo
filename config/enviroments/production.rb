2ac6406ae6a0799e9db2248842bc7396195a7961
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-09-09 12:11:03 +0100

    add rails configs

---
+++ config/enviroments/production.rb
@@ -0,0 +1,10 @@
+Rizzo::Application.configure do
+  config.cache_classes = true
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+  config.serve_static_assets = false
+  config.assets.compress = true
+  config.assets.compile = false
+  config.assets.digest = true
+  config.active_support.deprecation = :notify
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rizzo::Application.config.secret_token = 'e7be74c89e72a13c3f627e3ef522dbc766d014eaf353e728dc5a55da5bcfc53244072762d2302ddc0bfb49b9c2d535f46357c39df2ca8dcdf0e408bfc28492ec'  if defined?(Rizzo::Application)
