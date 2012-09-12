f1aa288bf1ad1433cd54dc637cc245b71063c67e
dave@kapoq.com <dave@kapoq.com>
2012-09-12 11:54:30 +0100

    Clean up boot and enviroNments

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
-require "active_resource/railtie"
-require 'sprockets'
 require "sprockets/railtie"
 
 if defined?(Bundler)
-  Bundler.require :default, Rails.env
+  Bundler.require(*Rails.groups(:assets => %w(development test)))
 end
 
 module Rizzo
