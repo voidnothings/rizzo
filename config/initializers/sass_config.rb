57463e0cf8cef053edb27afabd9566167ff59745
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-09-12 17:04:48 +0100

    remove js and sass sprockets config

--- config/initializers/sass_config.rb
+++
@@ -1,8 +0,0 @@
-if Gem::Specification.find_by_name("beaker") 
-  beaker_path = Gem::Specification.find_by_name("beaker").gem_dir
-  [File.join(beaker_path,'app','assets','stylesheets'),File.join(beaker_path,'vendor','assets','stylesheets')].each do |f|
-    Rails.application.assets.append_path(f)
-  end
-  # p 'Beaker assets path exposed to sprockets'
-end
-#
