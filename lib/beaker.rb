cdd68cfcd7bb0a54d6cd191d3c3fea779fc78e0c
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-09-13 11:55:02 +0100

    remove any sass initialization within rails engine boot

--- lib/beaker.rb
+++ lib/beaker.rb
@@ -5,10 +5,8 @@ module Beaker
     class Engine < ::Rails::Engine
 
       initializer "beaker.configure_rails_initialization" do |app|
-	require "sass/plugin"
-      
-        Sass::Plugin.add_template_location File.join(Gem.loaded_specs['beaker'].full_gem_path, '/app/assets/stylesheets')
-      
+ 
+
       end
 
     end
