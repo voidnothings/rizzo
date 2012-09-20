fe7c3779a69262c1934f6fac6f4f28dcbcb5880e
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-09-20 17:34:00 +0100

    normalize global routes

--- lib/rizzo/engine.rb
+++ lib/rizzo/engine.rb
@@ -4,10 +4,10 @@ module Rizzo
     initializer "rizzo.configure_rails_initialization" do |app|
 
       app.routes.prepend do
-        match 'global'           => 'global_resources#index'
-        match 'global_head'      => 'global_resources#head'
-        match 'global_header'    => 'global_resources#header'
-        match 'global_footer'    => 'global_resources#footer'
+        match 'global' => 'global_resources#index'
+        match 'global-head' => 'global_resources#head'
+        match 'global-body-header' => 'global_resources#header'
+        match 'global-body-footer' => 'global_resources#footer'
         match "r/:encrypted_url" => 'redirector#show', :as => :redirector
       end
 
