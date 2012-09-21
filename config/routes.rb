23cd69248c9e1aea4378e7e98db5c8148a4b99cf
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-09-21 11:38:27 +0100

    remove unused routes from rails engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rizzo::Application.routes.draw do
   match 'head' => 'head#index'  
-  match 'global'           => 'global_resources#index'
-  match 'global-head'      => 'global_resources#head'
+  match 'global'                => 'global_resources#index'
+  match 'global-head'           => 'global_resources#head'
   match 'global-body-header'    => 'global_resources#header'
   match 'global-body-footer'    => 'global_resources#footer'
-  match 'breadcrumb'       => 'global_resources#breadcrumb'
-  match "r/:encrypted_url" => 'redirector#show', :as => :redirector
+  match 'breadcrumb'            => 'global_resources#breadcrumb'
+  match "r/:encrypted_url"      => 'redirector#show', :as => :redirector
 end if defined?(Rizzo::Application)
--- lib/rizzo/engine.rb
+++ lib/rizzo/engine.rb
@@ -4,10 +4,7 @@ module Rizzo
     initializer "rizzo.configure_rails_initialization" do |app|
 
       app.routes.prepend do
-        match 'global' => 'global_resources#index'
-        match 'global-head' => 'global_resources#head'
-        match 'global-body-header' => 'global_resources#header'
-        match 'global-body-footer' => 'global_resources#footer'
+        match 'breadcrumb'            => 'global_resources#breadcrumb'
         match "r/:encrypted_url" => 'redirector#show', :as => :redirector
       end
 
