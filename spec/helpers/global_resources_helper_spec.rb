be52bf7ba7d7b82e082e019ccaf895b45da10a3d
Anselmo da Silva <anselmo.da.silva@lonelyplanet.co.uk>
2012-10-23 16:09:13 +0100

    move secondary_nav helper method to global_resources_helper file

--- spec/helpers/global_resources_helper_spec.rb
+++ spec/helpers/global_resources_helper_spec.rb
@@ -2,30 +2,80 @@ require 'spec_helper'
 require 'global_resources_helper'
 
 describe GlobalResourcesHelper do
-  before do
-    class << helper
-      include Haml, Haml::Helpers
+
+  context "legacy global resources" do 
+
+    before do
+      class << helper
+        include Haml, Haml::Helpers
+      end
+      helper.init_haml_helpers
     end
-    helper.init_haml_helpers
+
+    it "should get a list of navigation items" do
+      helper.primary_navigation_items.map{|n|n[:title]}.should ==
+        ['Home','Destinations','Thorn Tree forum','Shop','Hotels','Flights','Insurance']
+    end
+
+    it "should render the cart tab item" do
+      helper.cart_item_element.should have_css('li[class="globalCartHead"]')
+    end
+
+    it "should render the membership tab" do
+      helper.membership_item_element.should have_css('li[class="signInRegister cartDivider"]')
+    end
+
+    it "should render a white angle button" do
+      arr = helper.arrow_button(color: 'white', title: 'Details and Booking')
+      arr.should have_css('div[class="whiteAngleButton"]')
+      arr.should have_css('a[class="lpButton2010"]', text: 'Details and Booking')
+    end
+
   end
 
-  it "should get a list of navigation items" do
-    helper.primary_navigation_items.map{|n|n[:title]}.should ==
-      ['Home','Destinations','Thorn Tree forum','Shop','Hotels','Flights','Insurance']
+  context "secondary nav-bar" do
+
+    before do
+      class << helper
+        include Haml, Haml::Helpers
+      end
+      helper.init_haml_helpers
+      @args = {
+        title: 'Lisbon',
+        current: 'b',
+        collection: [
+          {title: 'a', url:'/a'},
+          {title: 'b', url:'/b'},
+          {title: 'c', url:'/c'},
+          {title: 'd', url:'/d'}
+          ]
+      } 
+    end
+
+    it 'renders the secondary bar' do
+      helper.secondary_nav_bar(@args).should have_css('div.secondary')
+    end
+
+    it "renders the secondary navigation" do
+      helper.secondary_nav_bar(@args).should have_css('nav.secondary-nav')
+    end
+
+    it "renders a title section on the secondary nav-bar" do 
+      helper.secondary_nav_bar(@args).should have_css('div.secondary h1[class="head-title"]')
+    end
+
+    it "renders a list of navigation anchors" do 
+      helper.secondary_nav_bar(@args).should have_css("a[href='\/a']", text: 'a')
+    end
+
+    it "sets the current section" do 
+      helper.secondary_nav_bar(@args).should have_css("a[class='current']", text: 'b')
+      helper.secondary_nav_bar(@args).should_not have_css("a[class='current']", text: 'c')
+    end
+
+
   end
 
-  it "should render the cart tab item" do
-    helper.cart_item_element.should have_css('li[class="globalCartHead"]')
-  end
-
-  it "should render the membership tab" do
-    helper.membership_item_element.should have_css('li[class="signInRegister cartDivider"]')
-  end
-
-  it "should render a white angle button" do
-    arr = helper.arrow_button(color: 'white', title: 'Details and Booking')
-    arr.should have_css('div[class="whiteAngleButton"]')
-    arr.should have_css('a[class="lpButton2010"]', text: 'Details and Booking')
-  end
 
 end
+
