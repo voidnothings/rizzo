c2656ecfd7ad8ff7db752fe9f6ca59eb7521260e
Ian Feather <info@ianfeather.co.uk>
2013-01-30 09:18:58 +0000

    Deleted unneccessary partial, crushed sprutes and updated row markup

--- features/step_definitions/global_resources_steps.rb
+++ features/step_definitions/global_resources_steps.rb
@@ -11,14 +11,14 @@ Then /^the global\-body\-header response should have the correct content$/ do
   page.should have_selector 'div.row--leaderboard'
   page.should have_selector 'nav.nav--primary'
   page.should have_selector 'div.search--primary'
-  page.should have_selector 'nav.nav-primary--user'
+  page.should have_selector 'nav.nav--primary--user'
 end
 
 Then /^the secure global\-body\-header response should have the correct content$/ do
   page.should have_selector 'div.accessibility'
   page.should have_selector 'nav.nav--primary'
   page.should have_selector 'div.search--primary'
-  page.should have_selector 'nav.nav-primary--user'
+  page.should have_selector 'nav.nav--primary--user'
 end
 
 Then /^the global\-body\-footer should response have the correct content$/ do
