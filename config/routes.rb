cfdd9af5740eefd2a0b086cb32cb38a1af8468dd
Ian Feather <info@ianfeather.co.uk>
2013-04-11 10:57:08 +0100

    Removed legacy (green header) routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rizzo::Application.routes.draw do
   match 'breadcrumb'                   => 'global_resources#breadcrumb'
   match "r/:encrypted_url"             => 'redirector#show', :as => :redirector
 
-  match 'global-head'                  => 'global_resources#show', :defaults => { :snippet => "head" }
-  match 'global-body-header'           => 'global_resources#show', :defaults => { :snippet => "body_header" }
-  match 'global-body-footer'           => 'global_resources#show', :defaults => { :snippet => "body_footer" }
-
+  # Core
   match 'client-solutions/global-head'        => 'global_resources#show', :defaults => { :snippet => "head", :cs => "true" }
   match 'client-solutions/global-body-header' => 'global_resources#show', :defaults => { :snippet => "body_header", :cs => "true" }
   match 'client-solutions/global-body-footer' => 'global_resources#show', :defaults => { :snippet => "body_footer", :cs => "true" }
+
+  # Legacy
+  match 'global-head'                  => 'global_resources#show', :defaults => { :snippet => "head" }
+  match 'global-body-header'           => 'global_resources#show', :defaults => { :snippet => "body_header" }
+  match 'global-body-footer'           => 'global_resources#show', :defaults => { :snippet => "body_footer" }
 
   match 'noscript/global-head'         => 'global_resources#show', :defaults => { :snippet => "head", :noscript => "true"}
   match 'noscript/global-body-footer'  => 'global_resources#show', :defaults => { :snippet => "body_footer", :noscript => "true"}
@@ -21,17 +23,8 @@ Rizzo::Application.routes.draw do
 
   match 'secure/noscript/global-head'         => 'global_resources#show', :defaults => { :snippet => "head", :noscript => "true"}
   match 'secure/noscript/global-body-footer'  => 'global_resources#show', :defaults => { :snippet => "body_footer", :noscript => "true"}  
-
-  match 'legacy/global-head'                  => 'global_resources#show', :defaults => { :snippet => "head", :layout=>'legacy' }
-  match 'legacy/global-body-header'           => 'global_resources#show', :defaults => { :snippet => "body_header", :layout=>'legacy' }
-  match 'legacy/global-body-footer'           => 'global_resources#show', :defaults => { :snippet => "body_footer", :layout=>'legacy' }
-  match 'legacy/secure/global-head'           => 'global_resources#show', :defaults => { :snippet => "head", :secure => "true", :layout=>'legacy' }
-  match 'legacy/secure/global-body-header'    => 'global_resources#show', :defaults => { :snippet => "body_header", :secure => "true", :layout=>'legacy' }
-  match 'legacy/secure/global-body-footer'    => 'global_resources#show', :defaults => { :snippet => "body_footer", :secure => "true", :layout=>'legacy' }
   
   match 'global'                       => 'global_resources#index'
   match 'secure/global'                => 'global_resources#index', :defaults => { :secure => "true" }
-  match 'legacy/global'                => 'global_resources#index', :defaults => { :layout=>'legacy'}
-  match 'legacy/secure/global'         => 'global_resources#index', :defaults => { :secure => "true", :layout=>'legacy'}
 
 end if defined?(Rizzo::Application)
