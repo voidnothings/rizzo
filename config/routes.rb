5a62b51df4bd3a23f8e569be2eea5ca1887c6ca4
Drew Miller <Drew.Miller@lonelyplanet.com>
2013-05-02 14:00:11 +0100

    Add tynt to header, but don't show it on thorntree paths

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rizzo::Application.routes.draw do
 
   # Legacy
   match 'global-head'                  => 'global_resources#show', :defaults => { :snippet => "head" }
+  match 'global-head-thorntree'        => 'global_resources#show', :defaults => { :snippet => "head", :suppress_tynt => "true" }
   match 'global-body-header'           => 'global_resources#show', :defaults => { :snippet => "body_header", :scope => 'core' }
   match 'global-body-footer'           => 'global_resources#show', :defaults => { :snippet => "body_footer" }
 
@@ -18,6 +19,7 @@ Rizzo::Application.routes.draw do
   match 'noscript/global-body-footer'  => 'global_resources#show', :defaults => { :snippet => "body_footer", :noscript => "true"}
 
   match 'secure/global-head'           => 'global_resources#show', :defaults => { :snippet => "head", :secure => "true" }
+  match 'secure/global-head-thorntree' => 'global_resources#show', :defaults => { :snippet => "head", :secure => "true", :suppress_tynt => "true" }
   match 'secure/global-body-header'    => 'global_resources#show', :defaults => { :snippet => "body_header", :secure => "true" }
   match 'secure/global-body-footer'    => 'global_resources#show', :defaults => { :snippet => "body_footer", :secure => "true" }
 
