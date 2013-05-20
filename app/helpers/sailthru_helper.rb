08b5505faca1075b3f8f447f34f752740269761a
Paolo Zaccagnini <paozac@gmail.com>
2013-05-20 10:58:10 +0100

    Sailthru: make title tag optional

--- app/helpers/sailthru_helper.rb
+++ app/helpers/sailthru_helper.rb
@@ -2,19 +2,17 @@ module SailthruHelper
   # Generates the meta tags requires by Sailthru Horizon and adds the setup
   # script.
   #
+  # Sailthru documentation:
+  #
+  #   http://getstarted.sailthru.com/node/61
+  #
   # Options:
   #
-  # - title
-  # - tags
+  # - title: sailthru will use the default title tag otherwise
+  # - tags:  sailthru will use the keywords meta tag otherwise
   #
   def sailthru_tags(opts = {})
-    title = if opts[:title]
-      opts[:title]
-    elsif content_for?(:head_title)
-      yield(:head_title)
-    else
-      'Lonely Planet Travel Guides and Travel Information'
-    end
+    title = opts[:title]
 
     tags = if opts[:tags].kind_of?(Array)
       opts[:tags].join(', ')
@@ -24,7 +22,7 @@ module SailthruHelper
 
     content_for :meta do
       haml_tag(:meta, name: "sailthru.date",  content: Time.now)
-      haml_tag(:meta, name: "sailthru.title", content: title)
+      haml_tag(:meta, name: "sailthru.title", content: title) if title
       haml_tag(:meta, name: "sailthru.tags",  content: tags) if tags
     end
   end
