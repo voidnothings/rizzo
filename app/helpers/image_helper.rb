0e2dc0d2dbd7f4a99f7cfceedb0180ac150ce179
Ian <info@ianfeather.co.uk>
2013-09-09 12:04:20 +0100

    Update method name based on PR review

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -3,13 +3,13 @@ module ImageHelper
   def safe_image_tag(image_url, image_opts={}, load_opts={})
     return if image_url.blank?
     if load_opts[:lazyload]
-      commented_image_tag(image_tag(image_url, image_opts))
+      lazyloaded_image_tag(image_tag(image_url, image_opts))
     else
       image_tag(image_url, image_opts)
     end
   end
 
-  def commented_image_tag(image)
+  def lazyloaded_image_tag(image)
     html = raw("<div data-uncomment=true>")
     html += raw("<!-- #{image.to_s} -->")
     html += raw("</div>")
