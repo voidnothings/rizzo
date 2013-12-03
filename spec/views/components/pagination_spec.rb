1af86edd917d28d1adf4d5121dba1cf51108fb18
i-like-robots <matt@maketea.co.uk>
2013-12-03 11:57:24 +0000

    Don't use encoded paths in examples, leave HAML to deal with it

--- spec/views/components/pagination_spec.rb
+++ spec/views/components/pagination_spec.rb
@@ -148,34 +148,34 @@ describe "components/_pagination.html.haml" do
 
     it 'appends the given URL parameter if it does not already exist in the path' do
 
-      view.stub(properties: default_properties.merge( :path => '/path/to/page?foo=bar&amp;baz=qux', :param => 'page' ))
+      view.stub(properties: default_properties.merge( :path => '/path/to/page?foo=bar&baz=qux', :param => 'page' ))
 
       render
 
       links = Capybara.string(rendered).all('.pagination__numbers a.pagination__link').map { |el| el[:href] }
       links.should eq( [
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=2',
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=3',
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=4',
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=5'
+        '/path/to/page?foo=bar&baz=qux&page=2',
+        '/path/to/page?foo=bar&baz=qux&page=3',
+        '/path/to/page?foo=bar&baz=qux&page=4',
+        '/path/to/page?foo=bar&baz=qux&page=5'
       ] )
 
     end
 
     it 'replaces the given URL parameter if it already exists in the path' do
 
-      view.stub(properties: default_properties.merge( :path => '/path/to/page?foo=bar&amp;baz=qux&amp;page=5', :param => 'page' ))
-      # view.stub(properties: default_properties.merge( :path => '/path/to/page?foo=bar&amp;page=5&amp;baz=qux', :param => 'page' ))
-      # view.stub(properties: default_properties.merge( :path => '/path/to/page?page=5&amp;foo=bar&amp;baz=qux', :param => 'page' ))
+      view.stub(properties: default_properties.merge( :path => '/path/to/page?foo=bar&baz=qux&page=5', :param => 'page' ))
+      # view.stub(properties: default_properties.merge( :path => '/path/to/page?foo=bar&page=5&baz=qux', :param => 'page' ))
+      # view.stub(properties: default_properties.merge( :path => '/path/to/page?page=5&foo=bar&baz=qux', :param => 'page' ))
 
       render
 
       links = Capybara.string(rendered).all('.pagination__numbers a.pagination__link').map { |el| el[:href] }
       links.should eq( [
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=2',
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=3',
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=4',
-        '/path/to/page?foo=bar&amp;baz=qux&amp;page=5'
+        '/path/to/page?foo=bar&baz=qux&page=2',
+        '/path/to/page?foo=bar&baz=qux&page=3',
+        '/path/to/page?foo=bar&baz=qux&page=4',
+        '/path/to/page?foo=bar&baz=qux&page=5'
       ] )
 
     end
