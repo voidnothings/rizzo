cf659c59ac691efed10d9fe10cd978c746773fbc
Ian <info@ianfeather.co.uk>
2014-01-24 16:52:02 +0000

    Remove outer closure

--- app/assets/javascripts/lib/mixins/flyout.js
+++ app/assets/javascripts/lib/mixins/flyout.js
@@ -1,67 +1,63 @@
-(function() {
+define([ "jquery" ], function($) {
 
   "use strict";
 
-  define([ "jquery" ], function($) {
+  var asFlyout = function(args) {
+    var _this = this;
 
-    var asFlyout = function(args) {
-      var _this = this;
+    this.$facetCount = $(args.facet),
 
-      this.$facetCount = $(args.facet),
+    // Performs a deep search of the filters object
+    this.countFilters = function(filters) {
+      var count = 0,
 
-      // Performs a deep search of the filters object
-      this.countFilters = function(filters) {
-        var count = 0,
+      countFilter = function(value) {
+        if (value === true) {
+          return count++;
+        }
+        for (var i in value) {
+          countFilter(value[i]);
+        }
+      };
 
-        countFilter = function(value) {
-          if (value === true) {
-            return count++;
+      for (var property in filters) {
+        countFilter(filters[property]);
+      }
+
+      return count;
+    },
+
+    this.updateFilters = function(filters) {
+      var filtersApplied = _this.countFilters(filters) || "";
+      if (filtersApplied) {
+        filtersApplied = "(" + filtersApplied + ")";
+      }
+      return filtersApplied;
+    },
+
+    this.close = $("#js-row--content").on(":toggleActive/click", function(event, data) {
+      var target = event.target;
+      if (data.isActive) {
+        $(document).on("click.toggleActive", function(event) {
+          if (!$(event.target).closest(".js-filter-flyout").length) {
+            $("#js-row--content").trigger(":toggleActive/update", target);
+            $(document).off("click.toggleActive");
           }
-          for (var i in value) {
-            countFilter(value[i]);
-          }
-        };
+        });
+      } else {
+        $(document).off("click.toggleActive");
+      }
 
-        for (var property in filters) {
-          countFilter(filters[property]);
-        }
+    });
 
-        return count;
-      },
+    this.updateCount = $("#js-row--content").on(":cards/received", function(event, data, state) {
+      state && state.filters && _this.$facetCount.text(_this.updateFilters(state.filters));
+    });
 
-      this.updateFilters = function(filters) {
-        var filtersApplied = _this.countFilters(filters) || "";
-        if (filtersApplied) {
-          filtersApplied = "(" + filtersApplied + ")";
-        }
-        return filtersApplied;
-      },
+    return this;
 
-      this.close = $("#js-row--content").on(":toggleActive/click", function(event, data) {
-        var target = event.target;
-        if (data.isActive) {
-          $(document).on("click.toggleActive", function(event) {
-            if (!$(event.target).closest(".js-filter-flyout").length) {
-              $("#js-row--content").trigger(":toggleActive/update", target);
-              $(document).off("click.toggleActive");
-            }
-          });
-        } else {
-          $(document).off("click.toggleActive");
-        }
+  };
 
-      });
+  return asFlyout;
 
-      this.updateCount = $("#js-row--content").on(":cards/received", function(event, data, state) {
-        state && state.filters && _this.$facetCount.text(_this.updateFilters(state.filters));
-      });
-
-      return this;
-
-    };
-
-    return asFlyout;
-
-  });
-
-}).call(this);
+});
