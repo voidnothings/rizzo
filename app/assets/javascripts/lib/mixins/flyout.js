af9755ad5f3359851d25502e4d447b77f2c56eb3
Ian <info@ianfeather.co.uk>
2014-01-24 15:17:38 +0000

    Add specs for updating flyout facet

--- app/assets/javascripts/lib/mixins/flyout.js
+++ app/assets/javascripts/lib/mixins/flyout.js
@@ -1,62 +1,64 @@
 (function() {
 
-    define(["jquery"], function($) {
+  define(["jquery"], function($) {
 
-        var asFlyout = function(args) {
-            _this = this,
-            this.$facetCount = $(args.facet);
+    var asFlyout = function(args) {
+      _this = this,
+      this.$facetCount = $(args.facet),
 
-            this.countFilters = function(filters) {
-                var count = 0,
-                    countFilter;
+      this.countFilters = function(filters) {
+        var count = 0,
 
-                countFilter = function(value) {
-                    if (value === true) {
-                        return count++
-                    }
-                    for (var i in value) {
-                        countFilter(value[i])
-                    }
-                }
-
-                for (var property in filters) {
-                    countFilter(filters[property])
-                }
-                return count;
-            },
-
-            this.updateFilters = function(filters) {
-                filtersApplied = _this.countFilters(filters) || ""
-                if (filtersApplied) {
-                    filtersApplied = "(" + filtersApplied + ")";
-                }
-                return filtersApplied;
-            },
-
-            this.close = $('#js-row--content').on(':toggleActive/click', function(event, data) {
-                var target = event.target;
-                if (data.isActive) {
-                    $(document).one('click.toggleActive', function(event) {
-                        if (!$(event.target).closest('.js-filter-flyout').length) {
-                            $('#js-row--content').trigger(':toggleActive/update', target);
-                        }
-                    });
-                } else {
-                    $(document).off('click.toggleActive')
-                }
-
-            });
-
-            this.updateCount = $('#js-row--content').on(':cards/received', function(event, data, state) {
-                state && state.filters && _this.$facetCount.text(_this.updateFilters(state.filters))
-
-            });
-
-            return this;
-
+        countFilter = function(value) {
+          if (value === true) {
+            return count++
+          }
+          for (var i in value) {
+            countFilter(value[i])
+          }
         }
-        return asFlyout;
 
-    });
+        for (var property in filters) {
+          countFilter(filters[property])
+        }
+
+        return count;
+      },
+
+      this.updateFilters = function(filters) {
+        filtersApplied = _this.countFilters(filters) || ""
+        if (filtersApplied) {
+          filtersApplied = "(" + filtersApplied + ")";
+        }
+        return filtersApplied;
+      },
+
+      this.close = $('#js-row--content').on(':toggleActive/click', function(event, data) {
+        var target = event.target;
+        if (data.isActive) {
+          $(document).on('click.toggleActive', function(event) {
+            if (!$(event.target).closest('.js-filter-flyout').length) {
+              $('#js-row--content').trigger(':toggleActive/update', target);
+              $(document).off('click.toggleActive')
+            }
+          });
+        } else {
+          $(document).off('click.toggleActive')
+        }
+
+      });
+
+      this.updateCount = $('#js-row--content').on(':cards/received', function(event, data, state) {
+        state && state.filters &&
+          _this.$facetCount.text(_this.updateFilters(state.filters))
+      });
+
+      return this;
+
+    }
+
+    return asFlyout;
+
+  });
 
 }).call(this);
--- spec/javascripts/lib/mixins/flyout_spec.js
+++ spec/javascripts/lib/mixins/flyout_spec.js
@@ -1,53 +1,58 @@
 require(['jquery', 'public/assets/javascripts/lib/mixins/flyout.js'], function($, asFlyout) {
 
-    describe('Flyout Mixin', function() {
+  describe('Flyout Mixin', function() {
 
-        beforeEach(function() {
-            window.flyout = asFlyout.call({})
-        });
-
-        describe('Object', function() {
-            it('is defined', function() {
-                expect(flyout).toBeDefined();
-            });
-        });
-
-        describe('counts', function() {
-            it('regular filters', function() {
-                count = flyout.countFilters({
-                    foo: true,
-                    bar: true
-                })
-                expect(count).toBe(2)
-            }),
-            it('deep filters', function() {
-                count = flyout.countFilters({
-                    foo: true,
-                    bar: {
-                        x: true,
-                        y: true
-                    }
-                })
-                expect(count).toBe(3)
-            })
-        });
-
-        describe('updates the facet count', function() {
-
-            it('when there are no filters', function() {
-                beforeEach(function() {
-                    spyOn(flyout, "countFilters").andReturn(0);
-                })
-                expect(flyout.updateFilters(filters)).toBe("")
-            }),
-            it('when there are no filters', function() {
-                beforeEach(function() {
-                    spyOn(flyout, "countFilters").andReturn(2);
-                })
-                expect(flyout.updateFilters(filters)).toBe("(2)")
-            })
-
-        })
-
+    beforeEach(function() {
+      window.flyout = asFlyout.call({}, "#js-ttd-spec")
     });
+
+    describe('Object', function() {
+      it('is defined', function() {
+        expect(flyout).toBeDefined();
+      });
+    });
+
+    describe('counts', function() {
+      it('regular filters', function() {
+        count = flyout.countFilters({
+          foo: true,
+          bar: true
+        })
+        expect(count).toBe(2)
+      }),
+      it('deep filters', function() {
+        count = flyout.countFilters({
+          foo: true,
+          bar: {
+            x: true,
+            y: true
+          }
+        })
+        expect(count).toBe(3)
+      })
+    });
+
+    describe('updates the facet count', function() {
+
+      describe('when there are no filters', function(){
+        beforeEach(function() {
+          spyOn(flyout, "countFilters").andReturn(0);
+        });
+
+        it('clears the filter', function() {
+          expect(flyout.updateFilters()).toBe("")
+        });
+      });
+      describe('when there are two filters', function() {
+        beforeEach(function() {
+          spyOn(flyout, "countFilters").andReturn(2);
+        });
+        it('updates', function() {
+          expect(flyout.updateFilters()).toBe("(2)")
+        })
+      })
+
+    })
+
+  });
 });
