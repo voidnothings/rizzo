bacea49069ff506cd5e57460ff709eb113c20adc
ianfeather <info@ianfeather.co.uk>
2014-02-24 21:33:42 -0600

    Add jscs and jshint as pre-commit hooks
    - Fix jshint issues
    - Fix jscs style errors
    - Add git hooks setup step

--- app/assets/javascripts/lib/utils/polyfill.js
+++ app/assets/javascripts/lib/utils/polyfill.js
@@ -1,51 +1,61 @@
-function Empty(){}
+function Empty() {}
+
 if (!Function.prototype.bind) {
-    Function.prototype.bind = function bind(that) {
-        var target = this;
-        if (typeof target != "function") {
-            throw new TypeError("Function.prototype.bind called on incompatible " + target);
+
+  Function.prototype.bind = function bind(that) {
+
+    "use strict";
+
+    var target = this,
+        args,
+        binder,
+        boundLength,
+        boundArgs,
+        bound;
+
+    if (typeof target != "function") {
+      throw new TypeError("Function.prototype.bind called on incompatible " + target);
+    }
+
+    args = Array.prototype.slice.call(arguments, 1);
+
+    binder = function() {
+
+      if (this instanceof bound) {
+
+        var result = target.apply(
+          this,
+          args.concat(Array.prototype.slice.call(arguments))
+        );
+        if (Object(result) === result) {
+          return result;
         }
+        return this;
 
-        var args = _Array_slice_.call(arguments, 1);
-
-        var binder = function () {
-
-            if (this instanceof bound) {
-
-                var result = target.apply(
-                    this,
-                    args.concat(_Array_slice_.call(arguments))
-                );
-                if (Object(result) === result) {
-                    return result;
-                }
-                return this;
-
-            } else {
-                return target.apply(
-                    that,
-                    args.concat(_Array_slice_.call(arguments))
-                );
-
-            }
-        };
-
-        var boundLength = Math.max(0, target.length - args.length);
-
-        var boundArgs = [];
-        for (var i = 0; i < boundLength; i++) {
-            boundArgs.push("$" + i);
-        }
-
-        var bound = Function("binder", "return function(" + boundArgs.join(",") + "){return binder.apply(this,arguments)}")(binder);
-
-        if (target.prototype) {
-            Empty.prototype = target.prototype;
-            bound.prototype = new Empty();
-            // Clean up dangling references.
-            Empty.prototype = null;
-        }
-
-        return bound;
+      } else {
+        return target.apply(
+          that,
+          args.concat(Array.prototype.slice.call(arguments))
+        );
+      }
     };
+
+    boundLength = Math.max(0, target.length - args.length);
+
+    boundArgs = [];
+    for (var i = 0; i < boundLength; i++) {
+      boundArgs.push("$" + i);
+    }
+
+    bound = Function("binder", "return function(" + boundArgs.join(",") + "){return binder.apply(this,arguments)}")(binder);
+
+    if (target.prototype) {
+      Empty.prototype = target.prototype;
+      bound.prototype = new Empty();
+      // Clean up dangling references.
+      Empty.prototype = null;
+    }
+
+    return bound;
+  };
 }
