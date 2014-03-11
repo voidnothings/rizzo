f74c5e82d9bccea4f5d92e9330c18178d3e47950
i-like-robots <matt@maketea.co.uk>
2014-03-11 12:54:22 +0000

    Port ad extensions to their own modules

--- app/assets/javascripts/lib/core/ad_unit.js
+++ app/assets/javascripts/lib/core/ad_unit.js
@@ -1,15 +1,24 @@
-define([ "jquery" ], function($) {
+define([ "jquery", "lib/core/ads/interstitial", "lib/core/ads/double_mpu" ], function($, Interstitial, DoubleMPU) {
 
   "use strict";
 
   function AdUnit($target) {
     this.$target = $target;
+    this.$iframe = $target.find("iframe");
     this._init();
   }
 
   AdUnit.prototype._init = function() {
-    if (!this.isEmpty()) {
-      this.$target.closest(".is-closed").removeClass("is-closed");
+    if (this.isEmpty()) {
+      return;
+    }
+
+    this.$target.closest(".is-closed").removeClass("is-closed");
+
+    var extension = this.$target("extension");
+
+    if (extension && this.extensions[extension]) {
+      this.extensions[extension].call(this);
     }
   };
 
@@ -28,6 +37,28 @@ define([ "jquery" ], function($) {
   AdUnit.prototype.refresh = function() {
     var slot = this.$target.data("googleAdUnit");
     window.googletag.pubads().refresh([ slot ]);
+  };
+
+  AdUnit.prototype.extensions = {
+
+    wallpaper: function() {
+      var $link = this.$iframe.contents().find("#ad-link");
+
+      if ($link.length) {
+        this.$target.addClass("ad-interstitial");
+        this.extension = new Interstitial(this.$target, $link);
+      }
+    },
+
+    mpu: function() {
+      var $container = this.$target.closest(".js-card-ad");
+
+      if (this.$iframe.height() > $container.height()) {
+        $container.addClass("ad-doubleMpu");
+        this.extension = new DoubleMPU(this.$target, $container);
+      }
+    }
+
   };
 
   return AdUnit;
