26a4c0e68c67bb0e7ec5e08d9b24aa67ff61b91c
i-like-robots <matt@maketea.co.uk>
2014-03-17 10:55:28 +0000

    Update network ID

--- app/assets/javascripts/lib/core/ad_manager_v2.js
+++ app/assets/javascripts/lib/core/ad_manager_v2.js
@@ -2,7 +2,7 @@ define([ "jquery", "lib/core/ad_unit", "dfp" ], function($, AdUnit) {
 
   "use strict";
 
-  var networkID = 4817;
+  var networkID = 9885583;
 
   function AdManager(config) {
     this.$listener = $(config.$listener || "#js-row--content");
--- app/assets/javascripts/lib/core/ads/double_mpu.js
+++ app/assets/javascripts/lib/core/ads/double_mpu.js
@@ -11,24 +11,20 @@ define([ "jquery" ], function($) {
   DoubleMPU.prototype._init = function() {
     var $grid = this.$card.closest(".js-stack"),
         $cards = $grid.find(".js-card"),
+        index = $cards.index(this.$card),
         position = this.$card.position(),
         cardsPerRow = Math.floor( $grid.width() / $cards.filter(".card--single").width() ),
         placeholderHTML = "<div class='card card--double ad--placeholder js-card' />";
 
-    // Eliminate all cards preceding our ad element so we can place a dummy
-    // element at the nth position *after* the current one using .eq()
-    $cards
-      .slice( $cards.index(this.$card) )
-      .eq(cardsPerRow - 2)
-      .after(placeholderHTML);
+    // The placeholder is two cards wide
+    $cards.eq(index).before(placeholderHTML);
+    $cards.eq(index + (cardsPerRow - 2)).after(placeholderHTML);
 
-    this.$card
-      .before(placeholderHTML)
-      .css({
-        position: "absolute",
-        left: position.left,
-        top: position.top
-      });
+    this.$card.css({
+      position: "absolute",
+      left: position.left,
+      top: position.top
+    });
   };
 
   return DoubleMPU;
