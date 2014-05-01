27326a827b1298259d881e7cb076b0b554314dd6
i-like-robots <matt@maketea.co.uk>
2014-05-01 17:05:43 +0100

    Change parallax formula so BG position is directly correlated to viewport

--- app/assets/javascripts/lib/components/hero_parallax.js
+++ app/assets/javascripts/lib/components/hero_parallax.js
@@ -1,4 +1,5 @@
 define([ "jquery", "lib/utils/viewport_helper" ], function($, viewportHelper) {
+
   "use strict";
 
   var HeroParallax,
@@ -6,19 +7,24 @@ define([ "jquery", "lib/utils/viewport_helper" ], function($, viewportHelper) {
       _stopScroll,
       _frame,
       started = false,
-      speed = 15,
       $els;
 
   HeroParallax = function( args ) {
     this.$els = args.$els || $(".js-bg-parallax");
-    $(window).bind("scroll", $.proxy(this._onScroll, this));
+    $(window).on("scroll", $.proxy(this._onScroll, this));
   };
 
   HeroParallax.prototype._updateBg = function( i ) {
     var $el = this.$els.eq(i);
+
     if (viewportHelper.withinViewport($el)) {
-      var percent = 30 + ((($el.offset().top - _pageYOffset) * speed) / $el.height());
-      $el.css("backgroundPosition", "center " + percent + "%");
+      var bias = $el.hasClass("is-top") ? 50 : 0,
+          elementPosition = $el.offset().top - _pageYOffset,
+          ratio = 100 / (window.innerHeight + $el.height()),
+          elementBottomEdge = elementPosition + $el.height(),
+          bgPosition = (elementBottomEdge * ratio) + bias;
+
+      $el.css("backgroundPosition", "center " + (bgPosition > 100 ? 100 : bgPosition) + "%");
     }
   };
 
