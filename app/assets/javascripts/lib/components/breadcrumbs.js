e929c23fbba61bcb6167119e5757ead1b7eb7355
Remy Bach <remy@bach.me.uk>
2014-05-16 10:58:24 +0100

    Ditch _this in favour of .bind(this)

--- app/assets/javascripts/lib/components/breadcrumbs.js
+++ app/assets/javascripts/lib/components/breadcrumbs.js
@@ -8,8 +8,6 @@ define([ "jquery" ], function($) {
 
   "use strict";
 
-  var _this;
-
   // @args = {}
   // listener: {string} selector for the listener.
   function Breadcrumbs(args) {
@@ -26,22 +24,21 @@ define([ "jquery" ], function($) {
   // -------------------------------------------------------------------------
 
   Breadcrumbs.prototype.listen = function() {
-    _this = this;
-
-    this.$listener.on(":cards/received :page/received", function(e, data) {
-      if (data.place) {
-        _this._updateNavBar(data.place);
-      }
-      if (data.breadcrumbs) {
-        _this._updateBreadcrumbs(data.breadcrumbs);
-      }
-    });
-
+    this.$listener.on(":cards/received :page/received", this._handleReceived.bind(this));
   };
 
   // -------------------------------------------------------------------------
   // Private Functions
   // -------------------------------------------------------------------------
+
+  Breadcrumbs.prototype._handleReceived = function(e, data) {
+    if (data.place) {
+      this._updateNavBar(data.place);
+    }
+    if (data.breadcrumbs) {
+      this._updateBreadcrumbs(data.breadcrumbs);
+    }
+  };
 
   Breadcrumbs.prototype._updateNavBar = function(place) {
     $("#js-secondary-nav .place-title-heading").attr("href", "/" + place.slug).text(place.name);
