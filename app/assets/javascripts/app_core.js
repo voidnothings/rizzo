dc91982cf7291665201f51f3078357331c8656f0
i-like-robots <matt@maketea.co.uk>
2014-05-20 15:13:58 +0100

    Remove app secure core, use app core

--- app/assets/javascripts/app_core.js
+++ app/assets/javascripts/app_core.js
@@ -13,22 +13,24 @@ require([ "jquery" ], function($) {
 
     $(function() {
 
-      new Base;
+      var secure = window.location.protocol === "https:";
+
+      new Base({ secure: secure });
       new ScrollPerf;
 
       // Currently we can"t serve Flamsteed over https because of f.staticlp.com
       // New community is using this file rather than app_secure_core
       // https://trello.com/c/2RCd59vk/201-move-f-staticlp-com-off-cloudfront-and-on-to-fastly-so-we-can-serve-over-https
-      if (window.location.protocol !== "https:") {
+      if (!secure) {
         window.lp.fs = new Flamsteed({
           events: window.lp.fs.buffer,
           u: $.cookies.get("lpUid")
         });
-      }
 
-      require([ "sailthru" ], function() {
-        window.Sailthru.setup({ domain: "horizon.lonelyplanet.com" });
-      });
+        require([ "sailthru" ], function() {
+          window.Sailthru.setup({ domain: "horizon.lonelyplanet.com" });
+        });
+      }
 
     });
 
