77f60a2e98a964f3b63b3cbe4d43225ec998b716
i-like-robots <matt@maketea.co.uk>
2014-05-21 15:45:51 +0100

    DRY asset reveal util

--- app/assets/javascripts/lib/utils/asset_reveal.js
+++ app/assets/javascripts/lib/utils/asset_reveal.js
@@ -22,7 +22,7 @@ define([ "jquery" ], function($) {
         selector = e.data[1];
       }
 
-      _this._uncomment(elements, selector || "[data-uncomment]");
+      _this._findElements(elements, selector || "[data-uncomment]", _this._uncomment);
     });
 
     this.$el.on(":asset/uncommentScript", function(e, elements, selector) {
@@ -31,82 +31,66 @@ define([ "jquery" ], function($) {
         selector = e.data[1];
       }
 
-      _this._uncommentScript(elements, selector || "[data-script]");
+      _this._findElements(elements, selector || "[data-script]", _this._uncommentScript);
     });
 
     this.$el.on(":asset/loadBgImage", function(e, elements) {
-      _this._loadBgImage(elements);
+      _this._findElements(elements, ".rwd-image-blocker", _this._loadBgImage);
     });
 
     this.$el.on(":asset/loadDataSrc", function(e, elements) {
-      _this._loadDataSrc(elements);
+      _this._findElements(elements, "[data-src]", _this._loadDataSrc);
     });
+  };
+
+  AssetReveal.prototype._findElements = function(elements, selector, callback) {
+    var i, len,
+        $elements = $(elements);
+
+    if (!$elements.is(selector)) {
+      $elements = $elements.find(selector);
+    }
+
+    for (i = 0, len = $elements.length; i < len; i++) {
+      callback.call(this, $elements.eq(i));
+    }
   };
 
   AssetReveal.prototype._removeComments = function(html) {
     return html.replace("<!--", "").replace("-->", "");
   };
 
-  AssetReveal.prototype._uncomment = function(elements, selector) {
-    var i, len, inner, $element,
-        $elements = $(elements).find(selector);
-
-    for (i = 0, len = $elements.length; i < len; i++) {
-      $element = $elements.eq(i);
-      inner = this._removeComments($element.html());
-      $element.before(inner).remove();
-    }
+  AssetReveal.prototype._uncomment = function($element) {
+    var inner = this._removeComments($element.html());
+    $element.before(inner).remove();
   };
 
-  AssetReveal.prototype._uncommentScript = function(elements, selector) {
-    var i, len, inner, $element,
-        $elements = $(elements).find(selector);
-
-    for (i = 0, len = $elements.length; i < len; i++) {
-      $element = $elements.eq(i);
-      inner = this._removeComments($element.html());
-      $element.html(inner);
-    }
+  AssetReveal.prototype._uncommentScript = function($element) {
+    var inner = this._removeComments($element.html());
+    $element.html(inner);
   };
 
-  AssetReveal.prototype._loadBgImage = function(elements) {
-    var $elements = $(elements),
-        blockedBG = "rwd-image-blocker";
-
-    if ($elements.hasClass(blockedBG)) {
-      $elements.removeClass(blockedBG);
-    } else {
-      $elements.find("." + blockedBG).removeClass(blockedBG);
-    }
+  AssetReveal.prototype._loadBgImage = function($element) {
+    $element.removeClass("rwd-image-blocker");
   };
 
-  AssetReveal.prototype._loadDataSrc = function(elements) {
-    var i, len, data, prop, $element, $img,
-        $elements = $(elements);
+  AssetReveal.prototype._loadDataSrc = function($element) {
+    var $img, prop,
+        data = $element.data();
 
-    for (i = 0, len = $elements.length; i < len; i++) {
-      $element = $elements.eq(i);
-
-      if (!$element.data("src")) {
-        $element = $element.find("[data-src]");
-      }
-
-      data = $element.data();
-
-      if (!data) {
-        continue;
-      }
-
-      $img = $("<img />");
-
-      for (prop in data) {
-        if (data.hasOwnProperty(prop)) {
-          $img.attr(prop, data[prop]);
-        }
-      }
-
-      $element.replaceWith($img);
+    if (!data) {
+      return;
     }
+
+    $img = $("<img />");
+
+    for (prop in data) {
+      if (data.hasOwnProperty(prop)) {
+        $img.attr(prop, data[prop]);
+      }
+    }
+
+    $element.replaceWith($img);
   };
 
   return AssetReveal;
