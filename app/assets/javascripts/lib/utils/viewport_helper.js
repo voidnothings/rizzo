13d1229e328d56d9ce1c23b2cb3839480a0ee972
Remy Bach <remy@bach.me.uk>
2014-05-21 15:00:32 +0100

    Add viewport helper spec

--- app/assets/javascripts/lib/utils/viewport_helper.js
+++ app/assets/javascripts/lib/utils/viewport_helper.js
@@ -1,32 +1,47 @@
+// ------------------------------------------------------------------------------
+//
+// viewportHelper
+//
+// ------------------------------------------------------------------------------
+
 define([ "jquery" ], function($) {
+
   "use strict";
 
-  var win = $(window);
+  var viewportHelper = {};
 
-  return {
+  viewportHelper.viewport = function() {
+    var win = viewportHelper._getWindow();
 
-    viewport: function() {
-      return {
-        width: win.width(),
-        height: win.height(),
-        top: win.scrollTop(),
-        left: win.scrollLeft(),
-        right: win.scrollLeft() + win.width(),
-        bottom: win.scrollTop() + win.height()
-      };
-    },
-
-    withinViewport: function($el) {
-      var bounds,
-          viewport = this.viewport();
-
-      bounds = $el.offset();
-      bounds.right = bounds.left + $el.outerWidth();
-      bounds.bottom = bounds.top + $el.outerHeight();
-      return !(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom);
-
-    }
-
+    return {
+      width: win.width(),
+      height: win.height(),
+      top: win.scrollTop(),
+      left: win.scrollLeft(),
+      right: win.scrollLeft() + win.width(),
+      bottom: win.scrollTop() + win.height()
+    };
   };
+
+  viewportHelper.withinViewport = function($el) {
+    var bounds,
+        viewport = this.viewport();
+
+    bounds = $el.offset();
+    bounds.right = bounds.left + $el.outerWidth();
+    bounds.bottom = bounds.top + $el.outerHeight();
+    return !(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom);
+  };
+
+  // -------------------------------------------------------------------------
+  // Private Functions
+  // -------------------------------------------------------------------------
+
+  // We need this function so we can stub it out for testing.
+  viewportHelper._getWindow = function() {
+    return $(window);
+  };
+
+  return viewportHelper;
 
 });
