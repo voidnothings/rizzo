373d6fc93be81b55ef32561003b7e7b4f6d5d9b5
Remy Bach <remy@bach.me.uk>
2014-05-23 12:57:07 +0100

    Mixin-ise the viewport helper

--- app/assets/javascripts/lib/page/viewport_helper.js
+++ app/assets/javascripts/lib/page/viewport_helper.js
@@ -8,38 +8,40 @@ define([ "jquery" ], function($) {
 
   "use strict";
 
-  var asViewportHelper = function() {};
+  var asViewportHelper = function() {
 
-  asViewportHelper.viewport = function() {
-    var win = this._getWindow();
+    this.viewport = function() {
+      var win = this._getWindow();
 
-    return {
-      width: win.width(),
-      height: win.height(),
-      top: win.scrollTop(),
-      left: win.scrollLeft(),
-      right: win.scrollLeft() + win.width(),
-      bottom: win.scrollTop() + win.height()
+      return {
+        width: win.width(),
+        height: win.height(),
+        top: win.scrollTop(),
+        left: win.scrollLeft(),
+        right: win.scrollLeft() + win.width(),
+        bottom: win.scrollTop() + win.height()
+      };
     };
-  };
 
-  asViewportHelper.withinViewport = function($el) {
-    var bounds,
-        viewport = this.viewport();
+    this.withinViewport = function($el) {
+      var bounds,
+          viewport = this.viewport();
 
-    bounds = $el.offset();
-    bounds.right = bounds.left + $el.outerWidth();
-    bounds.bottom = bounds.top + $el.outerHeight();
-    return !(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom);
-  };
+      bounds = $el.offset();
+      bounds.right = bounds.left + $el.outerWidth();
+      bounds.bottom = bounds.top + $el.outerHeight();
+      return !(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom);
+    };
 
-  // -------------------------------------------------------------------------
-  // Private Functions
-  // -------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
+    // Private Functions
+    // -------------------------------------------------------------------------
 
-  // We need this function so we can stub it out for testing.
-  asViewportHelper._getWindow = function() {
-    return $(window);
+    // We need this function so we can stub it out for testing.
+    this._getWindow = function() {
+      return $(window);
+    };
+
   };
 
   return asViewportHelper;
